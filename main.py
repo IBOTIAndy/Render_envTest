371595dcb57e1856ac5959fcf3f51f358c97aa55
IBOTIAndy <visua64@gmail.com>
2023-01-15 23:09:22 +0800

    look dir in render.com

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import os
-with open('/etc/secrets/botToken.json', 'r', encoding = 'utf8') as jfile:
-    botTokenJson = json.load(jfile)
-jfile.close()
-print(botToken)
+import glob
+path = './**/*.*'
+for file in glob.glob(path, recursive=True):
+    print(file)
+
+#with open('/etc/secrets/botToken.json', 'r', encoding = 'utf8') as jfile:
+#    botTokenJson = json.load(jfile)
+#jfile.close()
+#print(botToken)
